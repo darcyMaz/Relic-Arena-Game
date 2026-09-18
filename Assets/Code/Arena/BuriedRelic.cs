2fde3e72a6dfac402020c9e93f8d0cd03f956b6a
Darcy Mazloum <92594097+darcyMaz@users.noreply.github.com>
2026-09-18 13:33:14 -0400

    Sound manager ready for implementation
    The sound manager accepts metal detecting noise but does not play it. It does not have an audio source or however that works

--- Assets/Code/Arena/BuriedRelic.cs
+++ Assets/Code/Arena/BuriedRelic.cs
@@ -2,18 +2,32 @@ using UnityEngine;
 
 public class BuriedRelic : MonoBehaviour
 {
-    private static float NearDistance;
-    private static float VeryCloseDistance;
+    /// <summary>
+    /// Distance at which those with Metal Detectors can pick up BuriedRelics.
+    /// </summary>
+    private static float _closeDistance;
+    
+    /// <summary>
+    /// Ensures the initialization of this distance only happens once.
+    /// </summary>
     private bool DistanceSetBool = false;
 
     private void Awake()
     {
         if (!DistanceSetBool) 
         {
-
+            InitDistance();
+            DistanceSetBool = true;
         }
     }
 
+    /// <summary>
+    /// Set the distance where the BuriedRelics beep vigorously and can be picked up.
+    /// </summary>
+    private void InitDistance()
+    {
+        _closeDistance = 2;
+    }
 
     /// <summary>
     /// When something is in this trigger, check if it has a metal detector and play its sound.
---
+++ Assets/Code/Arena/SoundManager.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private List<AudioClip> _ongoingMetalDetectors = new List<AudioClip>();
+
+    public async void PlayMetalDetector(AudioClip audioClip)
+    {
+        // If this audio clip is not in the ongoingMetalDetector list, then it is not already playing.
+        if (!_ongoingMetalDetectors.Contains(audioClip))
+        {
+            // Add it to the list so the audio clip will not overlap with itself.
+            _ongoingMetalDetectors.Add(audioClip);
+
+            // Get the duration of the audio clip.
+            int audioClipDuration = 0;
+
+            // PLAY the audio clip.
+            Debug.Log("Sound Manager tried to play Metal Detector noises but the Sound Management was not implemented.");
+
+            await Task.Delay(audioClipDuration);
+
+            _ongoingMetalDetectors.Remove(audioClip);
+        }
+
+    }
+}
